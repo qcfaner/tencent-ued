4e014f5935ffcac130cf8c8797b4b5e638cd6c99
Zhanglei <Zhanglei@TymoncdeMacBook-Pro.local>
2020-06-06 23:13:01 +0800

    更新

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -3,33 +3,33 @@
     <div class="nav-content">
       <div class="nav-logo">
         <a href="javascript:;">
-          <img src="@/assets/image/top-nav/nav-logo.jpg" alt="官方链接" />
+          <img class="nav-pic" src="@/assets/image/top-nav/nav-logo.jpg" alt="官方链接" />
         </a>
       </div>
       <div class="nav-banner">
-        <a href="javascript:;">
+        <router-link to="/article">
           文章
-          <span>Atricle</span>
-        </a>
-        <span class="mark">/</span>
-        <a href="javascript:;">
+          <span>Article</span>
+        </router-link>
+        <span class="line">/</span>
+        <router-link to="/example">
           案例
           <span>Example</span>
-        </a>
-        <span class="mark">/</span>
-        <a href="javascript:;">
+        </router-link>
+        <span class="line">/</span>
+        <router-link to="/jobs">
           招聘
           <span>Jobs</span>
-        </a>
-        <span class="mark">/</span>
-        <a href="javascript:;">
+        </router-link>
+        <span class="line">/</span>
+        <router-link to="/about">
           关于
           <span>About</span>
-        </a>
-        <a href="javascript:;">
+        </router-link>
+        <router-link to="/join">
           5汇
-          <span class="info">H5</span>
-        </a>
+          <span>H5</span>
+        </router-link>
       </div>
     </div>
   </div>
@@ -50,28 +50,48 @@ export default {
     justify-content: space-between;
     width: 960px;
     margin: 0 auto;
+    .nav-pic {
+      display: block;
+    }
     .nav-banner {
       height: 67px;
       color: #65b5ee;
       a {
         color: #fff;
         display: inline-block;
+        text-decoration: none;
         height: 67px;
         line-height: 67px;
         border-top: 3px solid #34a8f8;
         padding: 0 3px;
+        margin-left: 5px;
+        &:hover {
+          border-top: 3px solid #7dd600;
+        }
+        &:last-child {
+          background: url(~@/assets/image/top-nav/nav-bg.jpg) no-repeat;
+          width: 134px;
+          height: 70px;
+          text-align: center;
+          border-top: none;
+          position: relative;
+          top: -3px;
+          &:hover {
+            background-position: 0 -70px;
+            border-top: none;
+          }
+          span {
+            font-size: 16px;
+            color: #fff;
+          }
+        }
         span {
           font-size: 14px;
           color: #c0e4fe;
           font-family: 微软雅黑, 黑体;
         }
-        .info {
-          font-size: 16px;
-          color: white;
-        }
       }
-      .mark {
-        font-size: 16px;
+      .line {
         color: #65b5ee;
         padding: 0 10px;
       }
---
+++ src/views/about/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>关于模块</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
